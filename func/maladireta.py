94be955243185530cb2c172e1501110d4c1025f2
Amir Saleh <28244923+amirmsaleh@users.noreply.github.com>
2025-07-13 21:06:57 -0300

    Zap Simples

--- func/maladireta.py
+++ func/maladireta.py
@@ -8,7 +8,7 @@ Gera arquivo com mala direta
 Este arquivo é parte do programa IrP_CIP
 Para mais detalhes verifique os arquivos README e LICENSE
 
-Copyright (C) 2023 AMS
+Copyright © 2023 AMS
 """
 
 import pandas as pd
@@ -37,19 +37,20 @@ def gera_maladireta():
     # Faz a leitura do arquivo CSV com os dados
     cadastro = pd.read_csv(var_ini['cip']['arq_csv'])
     
-    # Define variável com lista de membros de saída
+    # Define variáveis com lista de membros de saída
     lista_membros = []
+    lista_membros_zapsimples = []
     
     for ind in cadastro.index:
         # Gera lista para mala direta excluindo os inativos
         if cadastro['Ativo'][ind] == 'Sim':
 
-            # Formatação para o formato suportado pelo ZapFácil para discagem
+            # Formatação para o formato suportado para discagem
             celular = cadastro['Celular com DDD'][ind]
             for caracter in '()- ':
                 celular = celular.replace(caracter, '')
             
-            # O ZapFácil não suporta parênteses
+            # Remove os parênteses
             celular_coord = cadastro['Celular do coordenador'][ind]
             for caracter in '()':
                 celular_coord = celular_coord.replace(caracter, '')
@@ -74,13 +75,29 @@ def gera_maladireta():
                 cadastro['Coordenador regional'][ind],   # Coord_Regional
                 celular_coord,                           # Contato_Coord
                 contato,                                 # Contato
-                ])           
-            
+            ])
+
+            lista_membros_zapsimples.append([
+                # campo Nome: tratamento + apelido
+                f"{cadastro['Tratamento'][ind]} {cadastro['Apelido'][ind]}",
+                # Campo Número: número do celular
+                contato,
+                # campo E-mail: URL
+                dados_cip(num_cip)['url'],
+                # campo Var1: CIP
+                num_cip,
+                # campo Var2: senha
+                cadastro['Senha'][ind],
+                # Campo Var3: regional, coordenador e contato do cordenador
+                f"Regional: {cadastro['Regional'][ind]} | "
+                f"Aos cuidados de {cadastro['Coordenador regional'][ind]}: "
+                f"{celular_coord}",
+            ])
+
         # Mostra o andamento na tela
         # print(cadastro['CIP'][ind], cadastro['Nome completo'][ind])
 
-    
-    # Gera arquivo CSV com a lista de membros
+    # Gera arquivo CSV com a mala direta
     campos = ['CIP', 'Tratamento', 'Apelido', 'Nome', 'URL', 'Senha',
               'Regional', 'CoordRegional', 'ContatoCoord', 'Contato']
     df = pd.DataFrame(lista_membros, columns = campos)
@@ -88,4 +105,13 @@ def gera_maladireta():
     destino = var_ini['cip']['dir_dados'] + '/' + 'mala_direta.csv'
     df.to_csv(destino, index = False)
     print("Mala direta com", len(df), "registros:",destino)
+
+    # Gera arquivo CSV para o ZapSimples
+    campos_zap_simples = ['Nome', 'Número', 'E-mail', 'Var1', 'Var2', 'Var3']
+    df_zap_simples = pd.DataFrame(lista_membros_zapsimples,
+                                  columns = campos_zap_simples)
+    df_zap_simples.sort_values('Nome', ascending=True, inplace=True)
+    destino = var_ini['cip']['dir_dados'] + '/' + 'mala_direta_zapsimples.csv'
+    df_zap_simples.to_csv(destino, index = False)
+    print("Mala direta com", len(df_zap_simples), "registros:",destino)
     
